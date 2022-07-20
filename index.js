7a6070036837232783b80dbb15c7621fec1306ca
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-07-20 18:11:07 +0200

    Create play function
    takes players selections and returns string that declares the winner

--- index.js
+++ index.js
@@ -7,4 +7,36 @@ function getComputerChoice() {
     return choices[choice];
 }
 
+function play(playerSelection, computerSelection) {
+    playerSelection = String(playerSelection).toLowerCase();
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'You Win! Rock beats Scissors';
+        } else if (computerSelection === 'rock') {
+            return 'Tie! Rock and Rock';
+        } else if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'You Lose! Scissors beats Paper';
+        } else if (computerSelection === 'rock') {
+            return 'You Win! Paper beats Rock';
+        } else if (computerSelection === 'paper') {
+            return 'Tie! Paper and Paper';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'scissors') {
+            return 'Tie! Scissors and Scissors';
+        } else if (computerSelection === 'rock') {
+            return 'You Lose! Rock beats Scissors';
+        } else if (computerSelection === 'paper') {
+            return 'You Win! Scissors beats Paper';
+        }
+    }
+
+}
+
 choice = getComputerChoice()
+play('rock',getComputerChoice())
